9246bde4224244c53e57b69c7f7563348845799b
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-16 17:25:24 +0100

    Expressions in JSX

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,10 +1,12 @@
 
 
 function App() {
+  const name = 'bilal';
   return (
     <div className="container">
       <h1> hello from react</h1>
-      <h2>h2 tag </h2>
+      <h2>h2 tag my name is {name}</h2>
+      <h2>here is expressions {1+1}</h2>
 
     </div>
   );
