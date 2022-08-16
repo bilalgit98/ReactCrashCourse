ffeec4a011372383ac575e5a7a1aaedc4cc9314f
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-16 18:22:03 +0100

    Styling

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
 const header = ({ title }) => {
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <button className="btn">Add</button>
     </header>
   );
 };
