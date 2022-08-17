c48272520afdc7ad2082ec8da97cd0fdc08549e9
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-17 17:12:24 +0100

    State & useState Hook

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,19 +1,20 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Appointment 1",
-    day: "Feb 11th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Appointment 2",
-    day: "Feb 11th at 2:30pm",
-    reminder: true,
-  },
-];
+import { useState } from "react";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Appointment 1",
+      day: "Feb 11th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Appointment 2",
+      day: "Feb 11th at 2:30pm",
+      reminder: true,
+    },
+  ]);
   return (
     <>
       {tasks.map((task) => (
