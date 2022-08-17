6291c39cc06cce7d19397b2f95286fceb3b313c0
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-17 23:44:01 +0100

    Toggle Reminder and Conditional Styling

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -23,12 +23,20 @@ const App = () => {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks Available!"
       )}
