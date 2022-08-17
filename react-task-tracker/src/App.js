4ea55427724a435c8671a7177ec860e85a061cb6
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-17 23:53:57 +0100

    Add Task Form

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -35,6 +36,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
