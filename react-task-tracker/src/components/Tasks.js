45ba55e60144800e0e7dbf7227168021eb45ebec
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-17 17:53:51 +0100

    Revert "Task Component"
    This reverts commit ba46853acd92370dbb12e0a244ba283f6c53e7ba.

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,9 +1,8 @@
-import Task from "./Task";
 const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} />
+        <h3 key={task.id}>{task.text}</h3>
       ))}
     </>
   );
